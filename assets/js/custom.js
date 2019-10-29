bc10d42d590360a15a774efd1ed12d78359c763e
Jessie S <jessie.sholler@gmail.com>
2019-10-29 17:06:09 -0400

    flex box formatting - descriptions added to Fetch-A-Friend, RPS-Multiplayer Game, S-I-S, Liri Bot, and Icecream Taster

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,6 +3,7 @@ $('.no-link-alert').hide();
 
 $('.no-link').on('click', function() {
     $('.no-link-alert').show(); 
+    
     setTimeout(function(){
         $('.no-link-alert').fadeOut('slow'); 
      }, 3000);
